e6953d1490394f4aec468589c526e5d6ff130719
Felipe Ristow <felipertw2@gmail.com>
2026-05-06 17:22:42 +0200

    fix: avoid undefined loadModel by removing bound facade

--- src/plugin.ts
+++ src/plugin.ts
@@ -2,14 +2,6 @@ import { registerPlugin } from "@capacitor/core";
 
 import type { CapacitorOnnxPlugin } from "./definitions";
 
-const CapacitorOnnxNative = registerPlugin<CapacitorOnnxPlugin>("CapacitorOnnx", {
+export const CapacitorOnnx = registerPlugin<CapacitorOnnxPlugin>("CapacitorOnnx", {
   web: () => import("./web/index").then((m) => new m.CapacitorOnnxWeb()),
 });
-
-export const CapacitorOnnx = {
-  loadModel: CapacitorOnnxNative.loadModel.bind(CapacitorOnnxNative),
-  run: CapacitorOnnxNative.run.bind(CapacitorOnnxNative),
-  release: CapacitorOnnxNative.release.bind(CapacitorOnnxNative),
-  clearModel: CapacitorOnnxNative.clearModel.bind(CapacitorOnnxNative),
-  clearAllCache: CapacitorOnnxNative.clearAllCache.bind(CapacitorOnnxNative),
-};
